e8bb5a1634b22b7e07c8fad8a00302a9604e894b
Vaibhav <vaibhav@microfocus.com>
2019-09-21 15:57:20 +0530

    Angular: Default & 404 Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { DepartmentComponent } from './department/department.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+
+const routes: Routes = [
+  // When your application loads this will be the default component
+  {path: '', component: HomeComponent}, 
+  { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'department', component: DepartmentComponent },
+  // In case of any other path PageNotFoundComponent will load
+  { path: '**', component: PagenotfoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
